f91378470d4dacf1f53f2b69e6b98bfeb6592e69
tom crisp <veselio@outlook.com>
2019-07-23 16:41:33 +0100

    dynamic header using props

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ import React from 'react';
 
 
 // import destructuring: - Text 
-import { Text, AppRegistry } from 'react-native';
+import { Text, AppRegistry, YellowBox } from 'react-native';
 
 // for custom components the path of the component must be specified
 import Header from './src/components/header';
+
+
+YellowBox.ignoreWarnings(['Remote debugger']);
 
 // COMPONENT:
 // components are objects that can be placed on screen on the device
@@ -30,7 +33,7 @@ const App = () => {
     // JSX is used to display things to the device
     return (
         // component nested - using JSX for the imported statement
-        <Header />
+        <Header headerText='Album Homepage' />
     );
 };
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,44 @@
 // import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // make a component
 // when creating a const: const name = () => ... 
 // it is storing a function inside of a variable
-const Header = () => {
+const Header = (props) => {
+// styles are referenced through destructuring
+    // note these are props that are used in the components referenced to
+    const { textStyle, viewStyle, layoutStyle } = styles;
+
+    // const styling = styles;
+    // styling.textStyle;
+    // styling.viewStyle;
+
     return (
-        <Text>Album Homepage</Text>
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
     );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        // justify content positions vertically
+        justifyContent: 'center',
+        // align items positions horizontally
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 5,
+        // shadowColor: 'black',
+        // shadowOffset: { width: 0, height: 10 },
+        // shadowOpacity: 0.5,
+        elevation: 7,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 30
+    }
 };
 
 // make the component available to other parts of the app
